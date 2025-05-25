6e3f669e5ccb22be6566d39cbb94b46a972de8a5
Khaled <54leds@gmail.com>
2025-05-25 16:53:21 +0300

    kjkjhjhkl

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def handle_download(video_url, download_type):
         '--max-sleep-interval', '90',
         '--no-check-formats',
         '--force-generic-extractor',
-        '--no-playlist'
+        '--no-playlist',
         '--user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
         '--output', output_template
     ]
